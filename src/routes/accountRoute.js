cfb44a54a1427c3b39ee8fd6e70ed62c67472ba7
DucNguyen <hongducmmo@gmail.com>
2024-11-15 23:05:02 +0700

    Add Auth Middleware

--- src/routes/accountRoute.js
+++ src/routes/accountRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
+const middleware = require('../middlewares/authMiddleware')
 
 // Định nghĩa các route cho các API
-router.get('/', accountController.getAllAccounts);          // Lấy tất cả các tài khoản
+router.get('/', middleware.authenticateUser, accountController.getAllAccounts);          // Lấy tất cả các tài khoản
 router.get('/:id', accountController.getAccountById);       // Lấy một tài khoản theo ID
 router.post('/', accountController.createAccount);          // Tạo mới một tài khoản
 router.put('/:id', accountController.updateAccount);        // Cập nhật một tài khoản
